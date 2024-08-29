fa37db4be344ca301510799b0b50c3161200e33a
sma-bari <mdazizulbarishafin@gmail.com>
2024-08-29 18:03:56 +0600

    type error fixing

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Popular from "./Components/Popular";
 import Movies from "./Components/Movies";
 import Tvshows from "./Components/Tvshows";
 import People from "./Components/People";
-import MovieDetails from "./Components/movieDetails";
+import MovieDetails from "./Components/MovieDetails";
 import TvDetails from "./Components/TvDetails";
 import PersonDetails from "./Components/PersonDetails";
 import Trailer from "./Components/partials/Trailer";
