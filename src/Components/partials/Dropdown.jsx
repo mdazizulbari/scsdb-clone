d483db2fb9ef4c7d40585cc6ea103387891635a0
sma-bari <mdazizulbarishafin@gmail.com>
2024-08-30 20:59:29 +0600

    dropdown improved

--- src/Components/partials/Dropdown.jsx
+++ src/Components/partials/Dropdown.jsx
@@ -1,12 +1,20 @@
 const Dropdown = ({ title, options, func }) => {
   return (
     <div className="select">
-      <select onChange={func} defaultValue="0" name="format" id="format">
+      <select
+        // className="px-4 py-1 text-gray-400 rounded-full bg-gray-800 border-0 hover:bg-[#6556CD] hover:text-white"
+        onChange={func}
+        defaultValue="0"
+        name="format"
+        id="format"
+      >
         <option value="0" disabled>
           {title}
         </option>
         {options.map((option, index) => (
-          <option key={index} value={option}>{option.toUpperCase()}</option>
+          <option key={index} value={option}>
+            {option.toUpperCase()}
+          </option>
         ))}
       </select>
     </div>
