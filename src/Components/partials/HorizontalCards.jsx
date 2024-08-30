07df4860c0a8adc190505fa132536bc930fc1ae0
sma-bari <mdazizulbarishafin@gmail.com>
2024-08-30 23:23:11 +0600

    fiexed the error of d

--- src/Components/partials/HorizontalCards.jsx
+++ src/Components/partials/HorizontalCards.jsx
@@ -1,20 +1,20 @@
 import { Link } from "react-router-dom";
 import noimage from "../../../public/imageNotFound.avif";
 
-const HorizontalCards = ({ data=[] }) => {
+const HorizontalCards = ({ data = [] }) => {
   return (
     <div className="w-full h-fit mb-5 p-5 pb-1 flex overflow-y-hidden overflow-x-scroll">
-      {data.length>0 ? (
+      {data.length > 0 ? (
         data.map((d, i) => (
           <Link
-            to={`/${d.media_type}/details/${d.id}`}
+            to={d && `/${d.media_type}/details/${d.id}`}
             className="min-w-64 mr-5 rounded-3xl transition-all ease-in duration-300 hover:bg-[#6556CD]"
             key={i}
-          > 
+          >
             <img
               className="w-64 h-36 object-cover rounded-3xl"
               src={
-                d.backdrop_path || d.profile_path || d.poster_path
+                d && (d.backdrop_path || d.profile_path || d.poster_path)
                   ? `https://image.tmdb.org/t/p/original/${
                       d.backdrop_path || d.profile_path || d.poster_path
                     }`
@@ -23,10 +23,11 @@ const HorizontalCards = ({ data=[] }) => {
             />
             <div className="p-3 text-white overflow-y-scroll">
               <h1 className="mt-1 text-xl text-center font-semibold">
-                {d.name || d.title || d.original_name || d.original_title}
+                {d &&
+                  (d.name || d.title || d.original_name || d.original_title)}
               </h1>
               <p className="text-sm text-center h-fit">
-                {d.overview.slice(0, 50)}...
+                {d && d.overview.slice(0, 50)}...
                 <span className="text-blue-400"> more</span>
               </p>
             </div>
