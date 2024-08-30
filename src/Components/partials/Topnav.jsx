b53057b65c353e1032f09ce911761f347bd4169a
sma-bari <mdazizulbarishafin@gmail.com>
2024-08-30 20:31:05 +0600

    topnav improved

--- src/Components/partials/Topnav.jsx
+++ src/Components/partials/Topnav.jsx
@@ -20,31 +20,31 @@ const Topnav = () => {
   }, [query]);
 
   return (
-    <div className="w-3/4 h-[8vh] mx-auto relative flex items-center">
-      <i className="ri-search-line text-3xl text-zinc-400"></i>
+    <div className="w-1/2 h-[8vh] mx-auto px-5 relative flex items-center justify-between rounded-full hover:bg-[#6556CD]">
+      <i className="ri-search-line text-3xl text-zinc-300 hover:text-white"></i>
       <input
         onChange={(e) => setQuery(e.target.value)}
         value={query}
         type="text"
         placeholder="Search Anything"
-        className="w-1/2 p-5 mx-10 text-xl text-zinc-400 outline-none border-none bg-transparent"
+        className="w-3/4 p-5 mx-10 text-xl text-zinc-400 outline-none border-none bg-transparent focus:text-white"
       />
       {query.length > 0 && (
         <i
           onClick={() => setQuery("")}
-          className="ri-close-line text-3xl text-zinc-400 right-0"
+          className="ri-close-line text-3xl text-zinc-300 right-0"
         ></i>
       )}
 
-      <div className="w-1/2 max-h-[50vh] z-10 absolute top-full left-[5%] bg-zinc-200 overflow-auto">
+      <div className="w-full mt-5 max-h-[50vh] z-10 absolute -left-[.1%] top-full rounded-3xl bg-zinc-800 overflow-auto">
         {searches.map((search, index) => (
           <Link
           to={`/${search.media_type}/details/${search.id}`}
             key={index}
-            className="w-full p-10 font-semibold flex justify-start items-center border-b-2 border-zinc-100 hover:text-black hover:bg-zinc-300 duration-300"
+            className="w-full p-10 font-semibold flex justify-start items-center border-b-2 border-zinc-700 text-zinc-400 hover:text-white hover:bg-[#6556CD] duration-300"
           >
             <img
-            className="w-[10vh] h-[10vh] mr-5 shadow-lg object-cover"
+            className="w-[14vh] h-[14vh] mr-5 shadow-lg object-cover rounded-3xl"
               src={
                 search.backdrop_path || search.profile_path
                   ? `https://image.tmdb.org/t/p/original/${
@@ -54,7 +54,7 @@ const Topnav = () => {
               }
               alt=""
             />
-            <span>
+            <span className="text-2xl">
               {search.name || search.title || search.original_name || search.original_title}
             </span>
           </Link>
